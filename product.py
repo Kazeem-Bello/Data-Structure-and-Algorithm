ce1a65ace76a09b75309ca33963fc9b94aeaea1a
Kazeem-Bello <bel.kazeem@gmail.com>
2025-12-14 23:59:46 +0000

    product api updated

--- product.py
+++ product.py
@@ -14,10 +14,10 @@ class ProductBase(BaseModel):
 class Product(ProductBase):
     id: int
 
-class ProductStock(BaseModel):
+class Productupdate(BaseModel):
     name: str | None = None
     description: str | None = None
-    price: str | None = None
+    price: int | None = None
     stock: int | None = None
 
 products = {}
@@ -46,7 +46,7 @@ async def get_product():
 @app.get("/product/{product_id}")
 async def get_product_by_id(product_id: int):
     if product_id not in products.keys():
-        raise HTTPException(status_code = 400, detail = "Product id not found")
+        raise HTTPException(status_code = 404, detail = "Product id not found")
     product = products[product_id]
     return product
 
@@ -54,10 +54,45 @@ async def get_product_by_id(product_id: int):
 async def get_product_by_stock(stock: int | None = None):
     stocks = [i["stock"] for i in products.values()]
     if stock not in stocks:
-        raise HTTPException(status_code = 401, detail = "No product has the provided stock")
+        raise HTTPException(status_code = 404, detail = "No product has the provided stock")
     product_stock = [product for product in products.values() if product["stock"] == stock ]
     # product_list = []
     # for product in products.values():
     #     if product["stock"] == stock:
     #         product_list.append(product)
     return product_stock
+
+@app.put("/update")
+async def update_product(product: Product):
+    if product.id not in products.keys():
+        raise HTTPException(status_code = 404, detail = "Product ID doesn't exist")
+    products[product.id] = {
+        "id": product.id,
+        "name": product.name,
+        "description": product.description,
+        "price": product.price,
+        "stock": product.stock
+    }
+    return products[product.id]
+
+@app.patch("/patial_update")
+async def partial_update(product_id: int, product_data:Productupdate):
+    if product_id not in products.keys():
+        raise HTTPException(status_code = 404, detail = "Product ID doesn't exist")
+    product = products[product_id]
+    if product_data.name is not None:
+        product["name"] = product_data.name    
+    if product_data.description is not None:
+        product["description"] = product_data.description
+    if product_data.price is not None:
+        product["price"] = product_data.price
+    if product_data.stock is not None:
+        product["stock"] = product_data.stock
+    return product
+
+@app.delete("/delete")
+async def delete_product(product_id: int):
+    if product_id not in products.keys():
+        raise HTTPException(status_code = 404, detail = "Product ID doesn't exist")
+    del products[product_id]
+    return {"message": "Product deleted successfully"}
